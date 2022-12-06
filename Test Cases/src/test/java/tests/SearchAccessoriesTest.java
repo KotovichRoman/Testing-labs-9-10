7d858a39911ff24bec55ff604da039ffb49bec12
Degradant <90028593+KotovichRoman@users.noreply.github.com>
2022-12-06 10:06:46 +0300

    Update SearchAccessoriesTest.java

--- Test Cases/src/test/java/tests/SearchAccessoriesTest.java
+++ Test Cases/src/test/java/tests/SearchAccessoriesTest.java
@@ -17,18 +17,22 @@ public class SearchAccessoriesTest {
         driver = new ChromeDriver();
     }   
 
-    @Test
+   @Test
     public void isNewPateCreated() throws InterruptedException {
+        Thread.sleep(2500);
         mainPage = new MainPage(driver);
+        Thread.sleep(2500);
         mainPage.OpenPage();
+        Thread.sleep(2500);
         mainPage.EnterTextToSearchField("bomber");
+        Thread.sleep(2500);
         mainPage.ChooseColorAndSize();
     }
 
-    // @AfterMethod(alwaysRun = true)
-    // public void browserTearDown() throws InterruptedException {
-    //     Thread.sleep(5000);
-    //     driver.quit();
-    //     driver = null;
-    // }
+    @AfterMethod(alwaysRun = true)
+    public void browserTearDown() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.quit();
+        driver = null;
+    }
 }
